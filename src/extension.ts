4614b4c75cba8d94b9887a83c898a00bd0efc570
Felipe Gasparelo <fagasparelo@sjrp34038627.sjrpretow.local>
2018-04-24 18:55:50 -0300

    ...

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,12 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
+/*
 import { OutlineHelper } from './outline-helper';
 import { JavaLanguageInterface } from './java-language-interface';
-import { LanguageInterface } from './language-interface';
+import { LanguageInterface } from './language-interface';*/
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,24 +14,21 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Ativando "codemapper"!');
+    
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.codemapper', () => {
         // The code you place here will be executed every time your command is executed
+        /*
         let editor: vscode.TextEditor|undefined = vscode.window.activeTextEditor;
-        if (editor && editor.document.languageId === 'java'){
+        if (editor && editor.document.languageId === 'java'){ //Verifica se é Java e instancia
             let language: LanguageInterface = new JavaLanguageInterface();
             let texto: vscode.TextDocument = editor.document;
             let outLineHelper: OutlineHelper = new OutlineHelper(language,texto);
-        }
-
-
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Codemapper Ativado');
+            //let windowOutine: Thenable<string|undefined> = vscode.window.showInformationMessage('Code Mapper');            
+        } */            
     });
 
     context.subscriptions.push(disposable);
--- src/outline-item.ts
+++ src/outline-item.ts
@@ -1,11 +1,34 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-import { ItemType } from './item-type';
+import { ItemType } from "./item-type";
 
 export class OutlineItem {
+  constructor(
+    private _itemName: string,
+    private _itemType: ItemType,
+    private _itemLineCode: number
+  ) {}
 
-    constructor(private itemName: string, private itemType: ItemType, private itemLineCode: number) {
+  get itemName(): string {
+    return this._itemName;
+  }
 
-    }
+  set itemName(i: string) {
+    this._itemName = i;
+  }
 
+  get itemType(): ItemType {
+    return this._itemType;
+  }
+
+  set itemType(i: ItemType) {
+    this._itemType = i;
+  }
+  get itemLineCode(): number {
+    return this._itemLineCode;
+  }
+
+  set itemLineCode(i: number) {
+    this._itemLineCode = i;
+  }
 }
